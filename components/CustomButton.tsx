4da4e5aba06c215fc48d61469fe929c0aa75429f
allankruger <allanfloreskruger@gmail.com>
2023-10-06 21:36:02 +0200

    feat: add CustomButton types and props

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      className={`custom-btn ${containerStyles}`}
+      disabled={false}
+      type={"button"}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
 };
 
 export default CustomButton;
